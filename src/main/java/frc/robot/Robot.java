f6fb576508883e15129c60abc848949730ccca96
Jason Mihalick <jason@jcraftsmen.com>
2019-02-18 15:09:33 -0500

    Initial commit

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,123 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the TimedRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the build.gradle file in the
+ * project.
+ */
+public class Robot extends TimedRobot {
+  private static final String kDefaultAuto = "Default";
+  private static final String kCustomAuto = "My Auto";
+  private String m_autoSelected;
+  private final SendableChooser<String> m_chooser = new SendableChooser<>();
+
+  private CANSparkMax rightGearBoxMasterMotor = new CANSparkMax(
+    22, MotorType.kBrushless);
+private CANSparkMax leftGearBoxMasterMotor = new CANSparkMax(
+    21, MotorType.kBrushless);
+
+
+private CANSparkMax rightGearBoxSlave1 = new CANSparkMax(
+    23, MotorType.kBrushless);
+private CANSparkMax leftGearBoxSlave1 = new CANSparkMax(
+    20, MotorType.kBrushless);
+
+    private RobotDrive robotDrive = new RobotDrive(leftGearBoxMasterMotor, rightGearBoxMasterMotor);
+    private XboxJoystick joystick = new XboxJoystick(0);
+
+  /**
+   * This function is run when the robot is first started up and should be
+   * used for any initialization code.
+   */
+  @Override
+  public void robotInit() {
+    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
+    m_chooser.addOption("My Auto", kCustomAuto);
+    SmartDashboard.putData("Auto choices", m_chooser);
+
+    this.rightGearBoxSlave1.follow(rightGearBoxMasterMotor);
+    this.leftGearBoxSlave1.follow(leftGearBoxMasterMotor);
+
+  }
+
+  /**
+   * This function is called every robot packet, no matter the mode. Use
+   * this for items like diagnostics that you want ran during disabled,
+   * autonomous, teleoperated and test.
+   *
+   * <p>This runs after the mode specific periodic functions, but before
+   * LiveWindow and SmartDashboard integrated updating.
+   */
+  @Override
+  public void robotPeriodic() {
+  }
+
+  /**
+   * This autonomous (along with the chooser code above) shows how to select
+   * between different autonomous modes using the dashboard. The sendable
+   * chooser code works with the Java SmartDashboard. If you prefer the
+   * LabVIEW Dashboard, remove all of the chooser code and uncomment the
+   * getString line to get the auto name from the text box below the Gyro
+   *
+   * <p>You can add additional auto modes by adding additional comparisons to
+   * the switch structure below with additional strings. If using the
+   * SendableChooser make sure to add them to the chooser code above as well.
+   */
+  @Override
+  public void autonomousInit() {
+    m_autoSelected = m_chooser.getSelected();
+    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
+    System.out.println("Auto selected: " + m_autoSelected);
+  }
+
+  /**
+   * This function is called periodically during autonomous.
+   */
+  @Override
+  public void autonomousPeriodic() {
+    switch (m_autoSelected) {
+      case kCustomAuto:
+        // Put custom auto code here
+        break;
+      case kDefaultAuto:
+      default:
+        // Put default auto code here
+        break;
+    }
+  }
+
+  /**
+   * This function is called periodically during operator control.
+   */
+  @Override
+  public void teleopPeriodic() {
+      runDrivetrainIfNeeded();
+  }
+
+  protected void runDrivetrainIfNeeded() {
+    this.robotDrive.arcadeDrive(this.joystick.getLeftAxis(), this.joystick.getRightAxis());
+
+  }
+  /**
+   * This function is called periodically during test mode.
+   */
+  @Override
+  public void testPeriodic() {
+  }
+}
---
+++ src/main/java/frc/robot/XboxJoystick.java
@@ -0,0 +1,118 @@
+package frc.robot;
+
+
+import edu.wpi.first.wpilibj.XboxController;
+
+
+public class XboxJoystick extends XboxController {
+    
+    public XboxJoystick() {
+        this(0);
+    }
+    
+    public XboxJoystick(int port) {
+        super(port);
+    }
+
+    public boolean isLeftBumperClicked() {
+        return this.getBumper(Hand.kLeft);
+    }
+
+    public int getLeftBumperButtonNumber() {
+        return 5;
+    }
+    
+    public boolean isRightBumperClicked() {
+        return this.getBumper(Hand.kRight);
+    }
+    
+    public int getRightBumperButtonNumber() {
+        return 6;
+    }
+    
+    public boolean isButtonAClicked() {
+        return this.getAButton();
+    }
+
+    public int getAButtonNumber() {
+        return 1;
+    }
+    
+    public boolean isButtonBClicked() {
+        return this.getBButton();
+    }
+
+    public int getBButtonNumber() {
+        return 2;
+    }
+    
+    public boolean isButtonXClicked() {
+        return this.getXButton();
+    }
+    
+    public int getXButtonNumber() {
+        return 3;
+    }
+    
+    public boolean isButtonYClicked() {
+        return this.getYButton();
+    }
+
+    public int getYButtonNumber() {
+        return 4;
+    }
+    
+    public boolean isBackButtonClicked() {
+        return this.getBackButton();
+    }
+
+    public int getBackButtonNumber() {
+        return 7;
+    }
+    
+    public boolean isStartButtonClicked() {
+        return this.getStartButton();
+    }
+    
+    public int getStartButtonNumber() {
+        return 8;
+    }
+    
+    public boolean isLeftJoystickClicked() {
+        return this.getStickButton(Hand.kLeft);
+    }
+
+    public int getLeftJoystickButtonNumber() {
+        return 9;
+    }
+    
+    public boolean isRightJoystickClicked() {
+        return this.getStickButton(Hand.kRight);
+    }
+    
+    public int getRightJoystickButtonNumber() {
+        return 10;
+    }
+    
+    public boolean isLeftTriggerClicked() {
+        return this.getTriggerAxis(Hand.kLeft) > 0.0;
+    }
+
+    public boolean isRightTriggerClicked() {
+        return this.getTriggerAxis(Hand.kRight) > 0.0;
+    }
+    
+    public double getLeftAxis() {
+        // Xbox controller outputs negative values for movement up and positive values for movement down,
+        // but we want postive values for movement up which controls forward speed of the robot.  Therefore,
+        // we invert the value.
+        return -1 * this.getRawAxis(1);
+    }
+    
+    public double getRightAxis() {
+        // Xbox controller outputs negative values for movement left and positive values for movement right,
+        // but arcade drive seems to expect the opposite.  I.e., turning left is positive, like you would have on the
+        // unit circle.
+        return -1 *this.getRawAxis(4);
+    }
+}
